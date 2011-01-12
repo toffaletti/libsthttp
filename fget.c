8e5a04b58a39edba14f1354248322c3d1a6bd79a
Jason Toffaletti <jason@topsy.com>
2011-01-11 17:09:33 -0800

    more work on http_stream api

--- fget.c
+++ fget.c
@@ -10,6 +10,7 @@
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
 struct http_stream {
+
   httpclient_parser *clp;
   http_response *resp;
   const gchar *transfer_encoding;
@@ -26,27 +27,51 @@ struct http_stream {
   size_t total_read;
   size_t chunk_read;
 
-};
+}; /* http_stream */
 
-struct http_stream *http_open(httpclient_parser *clp, http_response *resp, st_netfd_t nfd) {
+struct http_stream *http_stream_open(httpclient_parser *clp, http_response *resp, st_netfd_t nfd) {
   struct http_stream *s = (struct http_stream *)calloc(1, sizeof(struct http_stream));
   s->clp = clp;
   s->resp = resp;
   s->nfd = nfd;
-  s->blen = 4 * 1024;
+  s->blen = 8 * 1024;
   s->buf = g_malloc(s->blen);
 
-  // TODO: probably use enum for transfer encoding
-  s->transfer_encoding = http_response_header_getstr(resp, "Transfer-Encoding");
-  const gchar *content_length = http_response_header_getstr(resp, "Content-Length");
-  if (content_length) { s->content_size = strtoull(content_length, NULL, 0); }
-  fprintf(stderr, "transfer_encoding: %s\n", s->transfer_encoding);
-  s->start = resp->body;
-  s->end = resp->body + resp->body_length;
+  http_response_parser_init(s->resp, s->clp);
+  size_t bpos = 0;
+  do {
+    httpclient_parser_init(s->clp);
+    ssize_t nr = st_read(s->nfd, &s->buf[bpos], s->blen-bpos, ST_UTIME_NO_TIMEOUT);
+    fprintf(stderr, "nr: %zd\n", nr);
+    size_t pe = httpclient_parser_execute(s->clp, s->buf, bpos+nr, 0);
+    fprintf(stderr, "pe: %zu\n", pe);
+    if (httpclient_parser_has_error(s->clp)) {
+      fprintf(stderr, "parser error");
+      break;
+    }
+    if (!httpclient_parser_is_finished(s->clp)) {
+      s->blen += (4 * 1024);
+      g_assert(s->blen < (4 * 1024 * 1024));
+      s->buf = g_realloc(s->buf, s->blen);
+      bpos += nr;
+      fprintf(stderr, "bpos: %zu\n", bpos);
+      http_response_clear(s->resp);
+    }
+  } while (!httpclient_parser_is_finished(s->clp));
+
+  if (httpclient_parser_is_finished(s->clp) && !httpclient_parser_has_error(s->clp)) {
+    // TODO: probably use enum for transfer encoding
+    s->transfer_encoding = http_response_header_getstr(resp, "Transfer-Encoding");
+    const gchar *content_length = http_response_header_getstr(resp, "Content-Length");
+    if (content_length) { s->content_size = strtoull(content_length, NULL, 0); }
+    fprintf(stderr, "transfer_encoding: %s\n", s->transfer_encoding);
+    s->start = resp->body;
+    s->end = resp->body + resp->body_length;
+  }
   return s;
 }
 
-static ssize_t _http_read_chunked(struct http_stream *s, void *ptr, size_t size) {
+static ssize_t _http_stream_read_chunked(struct http_stream *s, void *ptr, size_t size) {
   fprintf(stderr, "start: %p end: %p len: %zd\n", s->start, s->end, s->end - s->start);
   fprintf(stderr, "chunk_read: %zu\n", s->chunk_read);
   if (s->chunk_read > 0 && s->chunk_read == s->resp->chunk_size) {
@@ -88,9 +113,12 @@ static ssize_t _http_read_chunked(struct http_stream *s, void *ptr, size_t size)
   return -1;
 }
 
-ssize_t http_read(struct http_stream *s, void *ptr, size_t size) {
+ssize_t http_stream_read(struct http_stream *s, void *ptr, size_t size) {
+  if (httpclient_parser_has_error(s->clp)) { return -1; }
+  g_assert(s->resp->body);
+
   if (g_strcmp0("chunked", s->transfer_encoding) == 0) {
-    return _http_read_chunked(s, ptr, size);
+    return _http_stream_read_chunked(s, ptr, size);
   } else {
     if (s->content_size > 0 && s->content_size == s->total_read) { return 0; }
     if (s->total_read == 0) {
@@ -113,14 +141,18 @@ ssize_t http_read(struct http_stream *s, void *ptr, size_t size) {
   return -1;
 }
 
+void http_stream_close(struct http_stream *s) {
+  g_assert(s->buf);
+  g_free(s->buf);
+  memset(s, 0, sizeof(struct http_stream));
+  free(s);
+}
+
 static void *do_get(void *arg) {
-  size_t blen = 8 * 1024;
-  char *buf = g_malloc(blen);
-  size_t bpos = 0;
   const char *error_at = NULL;
   char *uri_s = (char *)arg;
-  struct hostent *host;
   int status;
+  struct hostent *host;
 
   uri u;
   uri_init(&u);
@@ -184,49 +216,24 @@ static void *do_get(void *arg) {
     http_response resp;
     httpclient_parser clp;
 
-    http_response_parser_init(&resp, &clp);
-    do {
-      httpclient_parser_init(&clp);
-      ssize_t nr = st_read(rmt_nfd, &buf[bpos], blen-bpos, ST_UTIME_NO_TIMEOUT);
+    struct http_stream *s = http_stream_open(&clp, &resp, rmt_nfd);
 
-      //buf[bpos + nr - 1] = 0;
-      fprintf(stderr, "nr: %zd\n", nr);
-      size_t pe = httpclient_parser_execute(&clp, buf, bpos+nr, 0);
-      fprintf(stderr, "pe: %zu\n", pe);
-      if (httpclient_parser_has_error(&clp)) {
-        perror("parser error");
-        goto close;
-      }
-      if (!httpclient_parser_is_finished(&clp)) {
-        blen += (4 * 1024);
-        buf = g_realloc(buf, blen);
-        bpos += nr;
-        fprintf(stderr, "bpos: %zu\n", bpos);
-        http_response_clear(&resp);
-      }
-
-      if (blen > (4 * 1024 * 1024)) {
-        // too big
-        goto close;
-      }
-    } while (!httpclient_parser_is_finished(&clp));
-
-    fprintf(stderr, "body_length: %zu\n", resp.body_length);
-    GString *resp_data = http_response_data(&resp);
+    GString *resp_data = http_response_data(s->resp);
     fprintf(stderr, "resp: %s\n", resp_data->str);
     g_string_free(resp_data, TRUE);
 
-    struct http_stream *s = http_open(&clp, &resp, rmt_nfd);
     ssize_t total = 0;
+    char buf[4 * 1024];
     for (;;) {
-      ssize_t nr = http_read(s, buf, blen);
-      fprintf(stderr, "http_read nr: %zd\n", nr);
+      ssize_t nr = http_stream_read(s, buf, sizeof(buf));
+      fprintf(stderr, "http_stream_read nr: %zd\n", nr);
       if (nr == 0 || nr == -1) break;
+      fwrite(buf, sizeof(char), nr, stdout);
       total += nr;
     }
-    fprintf(stderr, "http_read total: %zu\n", total);
+    fprintf(stderr, "http_stream_read total: %zu\n", total);
 
-close:
+    http_stream_close(s);
     http_response_free(&resp);
     st_netfd_close(rmt_nfd);
     break;
@@ -235,194 +242,10 @@ close:
 done:
 
   uri_free(&u);
-  g_free(buf);
   ares_free_hostent(host);
 
   return NULL;
 }
-
-#if 0
-static void *do_get(void *arg) {
-  size_t blen = 8 * 1024;
-  char *buf = g_malloc(blen);
-  size_t bpos = 0;
-  const char *error_at = NULL;
-  char *uri_s = (char *)arg;
-  struct hostent *host;
-  int status;
-
-  uri u;
-  uri_init(&u);
-  printf("uri: %s\n", uri_s);
-  if (uri_parse(&u, uri_s, strlen(uri_s), &error_at) == 0) {
-    printf("uri_parse error: %s\n", error_at);
-    goto done;
-  }
-  uri_normalize(&u);
-  printf("h: %s\n", u.host);
-  printf("p: %u\n", u.port);
-  if (g_strcmp0(u.scheme, "http") == 0 && u.port == 0) {
-    u.port = 80;
-  }
-  status = st_gethostbyname_r(u.host, &host);
-
-  char **p = NULL;
-  for (p = host->h_addr_list; *p; p++)
-  {
-    char addr_buf[46] = "??";
-    inet_ntop(host->h_addrtype, *p, addr_buf, sizeof(addr_buf));
-    printf("%-32s\t%s", host->h_name, addr_buf);
-    puts("");
-
-    int sock;
-
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-      perror("socket");
-      goto done;
-    }
-
-    st_netfd_t rmt_nfd;
-    if ((rmt_nfd = st_netfd_open_socket(sock)) == NULL) {
-      perror("st_netfd_open_socket");
-      goto done;
-    }
-
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = host->h_addrtype;
-    addr.sin_port = htons(u.port);
-    memcpy(&addr.sin_addr, *p, host->h_length);
-    if (st_connect(rmt_nfd, (struct sockaddr*)&addr, sizeof(addr), ST_UTIME_NO_TIMEOUT) < 0) {
-      perror("st_connect");
-      st_netfd_close(rmt_nfd);
-      continue;
-    }
-
-    printf("connected\n");
-    printf("p: %s\n", u.path);
-    http_request req;
-    http_request_make(&req, "GET", u.path);
-    http_request_header_append(&req, "Host", u.host);
-    http_request_fwrite(&req, stdout);
-
-    GString *req_data = http_request_data(&req);
-    st_write(rmt_nfd, req_data->str, req_data->len, ST_UTIME_NO_TIMEOUT);
-
-    http_request_free(&req);
-    g_string_free(req_data, TRUE);
-
-    http_response resp;
-    httpclient_parser clp;
-
-    http_response_parser_init(&resp, &clp);
-    do {
-      httpclient_parser_init(&clp);
-      ssize_t nr = st_read(rmt_nfd, &buf[bpos], blen-bpos, ST_UTIME_NO_TIMEOUT);
-
-      //buf[bpos + nr - 1] = 0;
-      printf("nr: %zd\n", nr);
-      size_t pe = httpclient_parser_execute(&clp, buf, bpos+nr, 0);
-      printf("pe: %zu\n", pe);
-      if (httpclient_parser_has_error(&clp)) {
-        perror("parser error");
-        goto close;
-      }
-      if (!httpclient_parser_is_finished(&clp)) {
-        blen += (4 * 1024);
-        buf = g_realloc(buf, blen);
-        bpos += nr;
-        printf("bpos: %zu\n", bpos);
-        http_response_clear(&resp);
-      }
-
-      if (blen > (4 * 1024 * 1024)) {
-        // too big
-        goto close;
-      }
-    } while (!httpclient_parser_is_finished(&clp));
-
-    printf("body_length: %zu\n", resp.body_length);
-    GString *resp_data = http_response_data(&resp);
-    printf("resp: %s\n", resp_data->str);
-    g_string_free(resp_data, TRUE);
-
-    const gchar *transfer_encoding = http_response_header_getstr(&resp, "Transfer-Encoding");
-    const gchar *content_length = http_response_header_getstr(&resp, "Content-Length");
-    size_t content_size = 0;
-    if (content_length) { content_size = strtoull(content_length, NULL, 0); }
-    printf("transfer_encoding: %s\n", transfer_encoding);
-    if (g_strcmp0("chunked", transfer_encoding) == 0) {
-      memmove(buf, resp.body, resp.body_length);
-      bpos = resp.body_length;
-
-parser_init:
-      http_response_clear(&resp);
-      httpclient_parser_init(&clp);
-      httpclient_parser_execute(&clp, buf, bpos, 0);
-      printf("\n=====\n");
-      printf("nread: %zu\n", clp.nread);
-      printf("chunk_size: %zu\n", resp.chunk_size);
-      printf("last_chunk: %d\n", resp.last_chunk);
-      printf("error?: %d\n", httpclient_parser_has_error(&clp));
-      printf("finished?: %d\n", httpclient_parser_is_finished(&clp));
-      if (resp.last_chunk || httpclient_parser_has_error(&clp)) {
-        goto close;
-      }
-      // consume(body, min(resp.chunk_size, resp.body_length));
-      if (bpos >= resp.chunk_size+2) {
-        bpos = bpos-resp.chunk_size-2;
-        memmove(buf, &resp.body[resp.chunk_size+2], bpos);
-        goto parser_init;
-      }
-
-      size_t total_read = bpos - (clp.nread-1);
-      while (total_read < resp.chunk_size+2) { // +2 for crlf
-        size_t nr = st_read(rmt_nfd, buf, blen, ST_UTIME_NO_TIMEOUT);
-        // consume(buf, nr);
-        total_read += nr;
-        bpos = nr;
-      }
-
-      if (total_read > resp.chunk_size+2) {
-        // number of bytes in the buffer that are past this chunk
-        size_t extra = (total_read - resp.chunk_size) - 2;
-        memmove(buf, &buf[bpos - extra], extra);
-        bpos = extra;
-        goto parser_init;
-      }
-
-      if (resp.chunk_size) {
-        size_t nr = st_read(rmt_nfd, buf, blen, ST_UTIME_NO_TIMEOUT);
-        bpos = nr;
-        goto parser_init;
-      }
-    } else {
-      size_t total_read = resp.body_length;
-      while (content_size == 0 || total_read < content_size) {
-        ssize_t nr = st_read(rmt_nfd, buf, blen, ST_UTIME_NO_TIMEOUT);
-        if (nr <= 0) break;
-        total_read += nr;
-        printf("read %zu bytes, %zu/%zu\n", nr, total_read, content_size);
-        if (content_size && total_read >= content_size) break;
-      }
-      printf("total_read: %zu\n", total_read);
-    }
-
-close:
-    http_response_free(&resp);
-    st_netfd_close(rmt_nfd);
-    break;
-  }
-
-done:
-
-  uri_free(&u);
-  g_free(buf);
-  ares_free_hostent(host);
-
-  return NULL;
-}
-#endif
 
 int main(int argc, char *argv[]) {
   int status;
