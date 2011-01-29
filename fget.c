dd8efc6ac2f48e45efb1510bd1de5ca25170c61e
Jason Toffaletti <jason@topsy.com>
2011-01-28 17:58:42 -0800

    update fget

--- fget.c
+++ fget.c
@@ -19,17 +19,18 @@ static void *do_get(void *arg) {
   fprintf(stderr, "h: %s\n", u.host);
   fprintf(stderr, "p: %u\n", u.port);
 
-  if (!http_stream_connect(s, u.host, u.port)) goto done;
-  if (!http_stream_request_init(s, "GET", &u)) goto done;
-  if (!http_stream_request_send(s)) goto done;
-  if (!http_stream_response_read(s)) goto done;
+  if (http_stream_connect(s, u.host, u.port) != HTTP_STREAM_OK) goto done;
+  http_stream_request_init(s, "GET", &u);
+  if (http_stream_request_send(s) != HTTP_STREAM_OK) goto done;
+  if (http_stream_response_read(s) != HTTP_STREAM_OK) goto done;
 
   size_t total = 0;
   char buf[4 * 1024];
   for (;;) {
-    ssize_t nr = http_stream_read(s, buf, sizeof(buf));
+    ssize_t nr = sizeof(buf);
+    int status = http_stream_read(s, buf, &nr);
     fprintf(stderr, "http_stream_read nr: %zd\n", nr);
-    if (nr == 0 || nr == -1) break;
+    if (nr <= 0 || status != HTTP_STREAM_OK) break;
     fwrite(buf, sizeof(char), nr, stdout);
     total += nr;
   }
